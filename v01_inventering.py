d650de4089ea813fb1db4d9e53199c7d26511d18
felixarvid2 <felixarvid@gmail.com>
2026-08-19 10:39:16 +0200

    lade till device 3 en brandvägg

--- v01_inventering.py
+++ v01_inventering.py
@@ -7,12 +7,17 @@ device_2 = "R-felix-1"
 model_2 = "CISCO2951"
 role_2 = "Router, lager 3"
 
+device_3 = "FW-felix-1"
+model_3 = "FPR-1010"
+role_3= "Brandvägg"
+
 #här skriver den rubriken
 print("UTRUSTNINGSLISTA")
 print("-" * 52)
 
 print(f"{device_1:<16} {model_1:<20} {role_1}")
 print(f"{device_2:<16} {model_2:<20} {role_2}")
+print(f"{device_3:<16} {model_3:<20} {role_3}")
 
 print("-" * 52)
-print("Antal enheter: 2")
+print("Antal enheter: 3")

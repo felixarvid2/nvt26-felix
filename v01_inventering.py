cd10c2fdcfa8fbb51a883a843408384c899cacb0
felixarvid2 <felixarvid@gmail.com>
2026-08-19 10:26:38 +0200

    uppdaterar med hela koden
    fortsättning med printfunktionen

--- v01_inventering.py
+++ v01_inventering.py
@@ -1,7 +1,18 @@
+# har inte några faktiska enheter så ävender mitt namn och samma modell som i läroboken
 device_1 = "SW-felix-1"
 model_1 = "WS-c3560G-48TS"
 role_1 = "Switch, access"
 
 device_2 = "R-felix-1"
 model_2 = "CISCO2951"
-role_2 = "Router"
+role_2 = "Router, lager 3"
+
+#här skriver den rubriken
+print("UTRUSTNINGSLISTA")
+print("-" * 52)
+
+print(f"{device_1:<16} {model_1:<20} {role_1}")
+print(f"{device_2:<16} {model_2:<20} {role_2}")
+
+print("-" * 52)
+print("Antal enheter: 2")

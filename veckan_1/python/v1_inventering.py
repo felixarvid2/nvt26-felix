56c0cf4e07f3adfc5175c4fce07be3f0fd0ac779
felixarvid2 <felixarvid@gmail.com>
2026-08-24 14:44:26 +0200

    Create v1_inventering.py

---
+++ veckan_1/python/v1_inventering.py
@@ -0,0 +1,23 @@
+# har inte några faktiska enheter så ävender mitt namn och samma modell som i läroboken
+device_1 = "SW-felix-1"
+model_1 = "WS-c3560G-48TS"
+role_1 = "Switch, access"
+
+device_2 = "R-felix-1"
+model_2 = "CISCO2951"
+role_2 = "Router, lager 3"
+
+device_3 = "FW-felix-1"
+model_3 = "FPR-1010"
+role_3= "Brandvägg"
+
+#här skriver den rubriken
+print("UTRUSTNINGSLISTA")
+print("-" * 53)
+
+print(f"{device_1:<16} {model_1:<20} {role_1}")
+print(f"{device_2:<16} {model_2:<20} {role_2}")
+print(f"{device_3:<16} {model_3:<20} {role_3}")
+
+print("-" * 53)
+print("Antal enheter: 3")

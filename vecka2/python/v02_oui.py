6d6b91a7720f91dff51ad8c7abf05c2ac08bf063
felixarvid2 <felixarvid@gmail.com>
2026-08-24 12:55:10 +0200

    Add vendor dictionary and MAC address processing
    Added a dictionary of vendors and a list of MAC addresses to identify manufacturers.

--- vecka2/python/v02_oui.py
+++ vecka2/python/v02_oui.py
@@ -1,1 +1,20 @@
-
+vendors = {
+    "a4:c3:f0": "Intel",
+    "3c:d9:2b": "Hewlett-Packard",
+    "00:1a:a1": "Cisco Systems",
+    "a8:2b:dd": " LCFC(Hefei) Electronics Technology co., ltd"
+}
+#byt ut mot riktiga mac adresser sen
+addresses = [
+    "a4:c3:f0:11:3a:b7",
+    "3c:d9:2b:2d:11:88",
+    "a8:2b:dd:dc:67:7e",
+    
+]
+for address in addresses:
+    prefix = address [0:8]
+    if prefix in vendors:
+        name = vendors[prefix]
+    else:
+        name = "okand tillverkare"
+    print (f"{address} -> {name}")
